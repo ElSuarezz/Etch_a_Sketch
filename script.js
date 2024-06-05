3cecc480f184312c9c6d898b7a6a48eda812ab1b
ElSuarezz <manuelmachadosoares@hotmail.com>
2024-06-05 20:14:33 +0000

    add new grid button and functionality.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const container = document.querySelector(".container");
+const resetBtn = document.getElementById('resetBtn');
+resetBtn.addEventListener('click', customGrid);
 
 
 function makeGrid(size){
-    
+    let boxSize = (402/size) + "px";
+
+    container.replaceChildren();
     for(i=0; i<(size*size); i++){
         const gridItem = document.createElement("div")
         gridItem.classList.add ("box")
         container.appendChild(gridItem);
+        gridItem.setAttribute('style', `width:${boxSize}; height:${boxSize}`);
         gridItem.addEventListener('mouseover', changeColor)
         gridItem.addEventListener('mouseout', eraseColor)
     }     
@@ -24,4 +29,17 @@ function eraseColor(e) {
         e.target.style.backgroundColor = 'white';
         
     }, 3000);
+}
+
+function customGrid() {
+    let input = prompt ('What will be your grid size? Choose from 1 to 100.')
+    let gridSize = parseInt(input)
+
+    if(gridSize < 1 || gridSize >100){
+        alert('You must choose between 1 and 100.')
+    
+    }else {
+        makeGrid(gridSize);
+    }
+
 }
